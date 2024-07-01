805112563a39f78e3a42b69ff02476c0cd037633
SafarSoFar <cafar0525@gmail.com>
2024-07-01 17:02:43 +0500

    readme, refactoring

---
+++ README.md
@@ -0,0 +1,18 @@
+# image2ascii
+
+An Image to ascii tool. Written in Rust.
+
+# Example #
+Original photo<br><br>
+![](example/funny-cat.jpg)<br><br>
+ASCII result<br><br>
+<img src="example/funny-cat-ascii.png" width="100" height="100"/>
+# Build #
+Build via cargo
+```
+cargo build
+```
+# Usage #
+```
+./image2ascii IMAGE_PATH
+```
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,38 @@
 use image::{self};
 use image::GenericImageView;
+use std::env;
 use std::fs::File;
 use std::io::Write;
-use std::str::Bytes;
 
+const  DENSITY : &str = "Ñ@#W$9876543210?!abc;:+=-,._";
 fn main() {
-    let density : &str = "Ñ@#W$9876543210?!abc;:+=-,._";
-    let density_vec : Vec<char> = density.chars().collect();
-    let img = image::open("funny-cat-resize.jpg").expect("no image was found");
+    let density_vec : Vec<char> = DENSITY.chars().collect();
+    let args : Vec<String> = env::args().collect();
+
+    if args.len() < 2{
+        panic!("\nError: provide an image path\n");
+    }
+    let img = image::open(&args[1]).expect("No image was found");
     let mut ascii_buf = String::new();
     let mut prev : u32 = 0;
     for pixel in img.pixels(){
         if prev != pixel.1{
             prev = pixel.1;
             ascii_buf.push('\n');
+            print!("\n");
         }
         let rgba = pixel.2;
         let mut average : f32 = 0.0;
         average += rgba[0] as f32;
         average += rgba[1] as f32;
         average += rgba[2] as f32;
-        average += rgba[3] as f32;
-        average /=  4.0;
+        average /=  3.0;
         let ind = (average/ 255.0 * 27.0) as usize;
+        print!("{}",density_vec[ind]);
         ascii_buf.push(density_vec[ind]);
     }
-    let mut file = File::create("ascii-output.txt").expect("couldn't create a file");
-    file.write_all(ascii_buf.as_bytes());
+    let mut file = File::create("ascii-output.txt").expect("Couldn't create a file");
+    file.write_all(ascii_buf.as_bytes()).expect("Couldn't write bytes to file");
+    println!("\n\nOutput was written to 'ascii-output.txt'\n");
 
 }
