a7fa940fdfc5316e4aaafd7c2b783aff9740f86e
SafarSoFar <cafar0525@gmail.com>
2024-07-01 14:56:21 +0500

    works

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,14 @@ use std::str::Bytes;
 fn main() {
     let density : &str = "Ñ@#W$9876543210?!abc;:+=-,._";
     let density_vec : Vec<char> = density.chars().collect();
-    println!("Hello, world!");
     let img = image::open("funny-cat-resize.jpg").expect("no image was found");
-    //println!("{}", 255/8);
-    let mut new_img = String::new();
+    let mut ascii_buf = String::new();
     let mut prev : u32 = 0;
     for pixel in img.pixels(){
         if prev != pixel.1{
-            new_img.push('\n');
+            prev = pixel.1;
+            ascii_buf.push('\n');
         }
-        //println!("{} - {}", pixel.0, pixel.1);
         let rgba = pixel.2;
         let mut average : f32 = 0.0;
         average += rgba[0] as f32;
@@ -24,13 +22,10 @@ fn main() {
         average += rgba[2] as f32;
         average += rgba[3] as f32;
         average /=  4.0;
-        //let r = (255-0)/(28-0);
-        //let y = (average) * r;
-        let ind = ((255.0-average) as f32 / 255.0 * 27.0) as usize;
-        new_img.push(density_vec[ind]);
-        //print!("{}", density_vec[ind]);
+        let ind = (average/ 255.0 * 27.0) as usize;
+        ascii_buf.push(density_vec[ind]);
     }
-    let mut file = File::create("bi.txt").expect("couldn't create a file");
-    file.write_all(new_img.as_bytes());
+    let mut file = File::create("ascii-output.txt").expect("couldn't create a file");
+    file.write_all(ascii_buf.as_bytes());
 
 }
